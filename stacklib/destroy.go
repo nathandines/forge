96adbc8b98ab1c34b272d8585815994a649b71dd
Nathan Dines <nath@ndin.es>
2018-03-05 00:03:39 +1100

    Improved event sequencing to cater for computers with inaccurate time

--- stacklib/destroy.go
+++ stacklib/destroy.go
@@ -4,13 +4,11 @@ import "github.com/aws/aws-sdk-go/service/cloudformation"
 
 // Destroy will delete the stack
 func (s *Stack) Destroy() (err error) {
-	if s.StackID == "" {
-		if err := s.GetStackID(); err != nil {
-			return err
-		}
+	if err := s.GetStackInfo(); err != nil {
+		return err
 	}
 	_, err = cfn.DeleteStack(&cloudformation.DeleteStackInput{
-		StackName: &s.StackName,
+		StackName: s.StackInfo.StackId,
 	})
 	return
 }
--- stacklib/events.go
+++ stacklib/events.go
@@ -17,14 +17,16 @@ func (t byTime) Swap(i, j int) { t[i], t[j] = t[j], t[i] }
 
 // Events will get all events for a stack and sort them in chronological order
 // within a time range
-func (s *Stack) Events(startTime, endTime *time.Time) (events []*cloudformation.StackEvent, err error) {
+func (s *Stack) Events(after *time.Time) (events []*cloudformation.StackEvent, err error) {
+	if err := s.GetStackInfo(); err != nil {
+		return events, err
+	}
 	if err := cfn.DescribeStackEventsPages(
 		&cloudformation.DescribeStackEventsInput{
-			StackName: &s.StackID,
+			StackName: s.StackInfo.StackId,
 		}, func(page *cloudformation.DescribeStackEventsOutput, lastPage bool) bool {
 			for _, e := range page.StackEvents {
-				if e.Timestamp.UnixNano() >= startTime.UnixNano() &&
-					e.Timestamp.UnixNano() < endTime.UnixNano() {
+				if e.Timestamp.UnixNano() > after.UnixNano() {
 					events = append(events, e)
 				}
 			}
@@ -38,4 +40,14 @@ func (s *Stack) Events(startTime, endTime *time.Time) (events []*cloudformation.
 	sort.Sort(byTime(events))
 
 	return
+}
+
+// GetLastEventTime will get the time of the last event for the stack
+func (s *Stack) GetLastEventTime() (*time.Time, error) {
+	epoch := time.Unix(0, 0)
+	events, err := s.Events(&epoch)
+	if err != nil {
+		return new(time.Time), err
+	}
+	return events[len(events)-1].Timestamp, err
 }
