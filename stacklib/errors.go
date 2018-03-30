309b6325e3cda29b69cd6ba175cd112d2a200c17
Nathan Dines <nath@ndin.es>
2018-03-31 00:46:26 +1100

    Require stack information to describe detail; testing

--- stacklib/errors.go
+++ stacklib/errors.go
@@ -3,3 +3,4 @@ package stacklib
 import "fmt"
 
 var errorNoStackID = fmt.Errorf("StackID must be defined. Hint: Use GetStackInfo() helper function")
+var errorNoStackNameOrID = fmt.Errorf("StackName or StackID must be defined")
