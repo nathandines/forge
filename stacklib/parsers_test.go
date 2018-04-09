8a4525e1dd60b8815722846b7968a0dbd7d5cd76
Nathan Dines <nath@ndin.es>
2018-04-09 21:25:28 +1000

    Added tests for Parameter parsing

--- stacklib/parsers_test.go
+++ stacklib/parsers_test.go
@@ -188,3 +188,248 @@ func TestParseTagsErrors(t *testing.T) {
 		}
 	}
 }
+
+func TestParseParameters(t *testing.T) {
+	cases := []struct {
+		input              string
+		expectedParameters []*cloudformation.Parameter
+	}{
+		// JSON String
+		{
+			input: `{"ThisKey":"ThisValue"}`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("ThisValue"),
+				},
+			},
+		},
+		// YAML String
+		{
+			input: "---\nThisKey: ThisValue",
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("ThisValue"),
+				},
+			},
+		},
+		// JSON Integer
+		{
+			input: `{"ThisKey":123}`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("123"),
+				},
+			},
+		},
+		// YAML Integer
+		{
+			input: "---\nThisKey: 123",
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("123"),
+				},
+			},
+		},
+		// JSON Float
+		{
+			input: `{"ThisKey": 3.1415926535897932384626433832795028841971}`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("3.141592653589793"),
+				},
+			},
+		},
+		// YAML Float
+		{
+			input: "---\nThisKey: 3.1415926535897932384626433832795028841971",
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("3.141592653589793"),
+				},
+			},
+		},
+		// JSON Bool
+		{
+			input: `{"ThisKey":true}`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("true"),
+				},
+			},
+		},
+		// YAML Bool
+		{
+			input: "---\nThisKey: true",
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("true"),
+				},
+			},
+		},
+		// JSON Misc-List
+		{
+			input: `{"ThisKey":["one","two"]}`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("one,two"),
+				},
+			},
+		},
+		// YAML String List
+		{
+			input: `---
+				ThisKey:
+				- one
+				- two`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("one,two"),
+				},
+			},
+		},
+		// JSON Misc-List
+		{
+			input: `{"ThisKey":["one",2,123.456,false]}`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("one,2,123.456,false"),
+				},
+			},
+		},
+		// YAML Misc-List
+		{
+			input: `---
+				ThisKey:
+				- one
+				- 2
+				- 123.456
+				- false`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("ThisKey"),
+					ParameterValue: aws.String("one,2,123.456,false"),
+				},
+			},
+		},
+		// JSON Multi-value
+		{
+			input: `{
+				"String": "Foobar",
+                "Int": 123,
+                "Float": 123.456,
+				"Boolean": true,
+				"List": ["one","two","three"]
+			}`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("String"),
+					ParameterValue: aws.String("Foobar"),
+				},
+				{
+					ParameterKey:   aws.String("Int"),
+					ParameterValue: aws.String("123"),
+				},
+				{
+					ParameterKey:   aws.String("Float"),
+					ParameterValue: aws.String("123.456"),
+				},
+				{
+					ParameterKey:   aws.String("Boolean"),
+					ParameterValue: aws.String("true"),
+				},
+				{
+					ParameterKey:   aws.String("List"),
+					ParameterValue: aws.String("one,two,three"),
+				},
+			},
+		},
+		// YAML Multi-value
+		{
+			input: `---
+                String: Foobar
+                Int: 123
+                Float: 123.456
+				Boolean: true
+				List:
+				- one
+				- two
+				- three`,
+			expectedParameters: []*cloudformation.Parameter{
+				{
+					ParameterKey:   aws.String("String"),
+					ParameterValue: aws.String("Foobar"),
+				},
+				{
+					ParameterKey:   aws.String("Int"),
+					ParameterValue: aws.String("123"),
+				},
+				{
+					ParameterKey:   aws.String("Float"),
+					ParameterValue: aws.String("123.456"),
+				},
+				{
+					ParameterKey:   aws.String("Boolean"),
+					ParameterValue: aws.String("true"),
+				},
+				{
+					ParameterKey:   aws.String("List"),
+					ParameterValue: aws.String("one,two,three"),
+				},
+			},
+		},
+	}
+
+	for i, c := range cases {
+		var parameters []*cloudformation.Parameter
+		parameters, err := parseParameters(c.input)
+		if err != nil {
+			t.Fatalf("%d, unexpected error, %v", i, err)
+		}
+
+	EXPECTED_PARAMETER:
+		for j := 0; j < len(c.expectedParameters); j++ {
+			e := struct{ ParameterKey, ParameterValue string }{
+				*c.expectedParameters[j].ParameterKey,
+				*c.expectedParameters[j].ParameterValue,
+			}
+			for k := 0; k < len(parameters); k++ {
+				g := struct{ ParameterKey, ParameterValue string }{
+					*parameters[k].ParameterKey,
+					*parameters[k].ParameterValue,
+				}
+				if e == g {
+					continue EXPECTED_PARAMETER
+				}
+			}
+			t.Errorf("%d, expected %+v, but not found in output parameters", i, e)
+		}
+	}
+}
+
+func TestParseParametersErrors(t *testing.T) {
+	cases := []string{
+		"bad:\nyaml",
+		`{"nested":{"objects":"here"}}`,
+		`["not","key","value","map"]`,
+		`{"Sublist":[["one","two"],"three"]}`,
+		`{"CommaInList":["foo,bar","foobar"]}`,
+	}
+
+	for i, c := range cases {
+		_, err := parseParameters(c)
+		if err == nil {
+			t.Fatalf("%d, expected error, but got success", i)
+		}
+	}
+}
